5632c74b475dd8bca2ade362cc91b3d04fc9ec85
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-04 00:19:01 +0200

    Add startup settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
@@ -15,5 +17,14 @@ const filename = (ext) => isDev
         path: path.resolve(__dirname, 'build'),
         filename: `./js/${filename('js')}`
     },
-
+     plugins: [
+         new HTMLWebpackPlugin({
+             template: path.resolve(__dirname, 'src/index.html'),
+             filename: 'index.html',
+             minify: {
+                 collapseWhitespace: isProd,
+             },
+         }),
+         new CleanWebpackPlugin(),
+     ]
   };
