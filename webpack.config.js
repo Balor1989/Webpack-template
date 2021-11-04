b6cc2bace3d76a1c5e568d5c048bde67463a6afb
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-04 09:27:57 +0200

    Add startup settigs

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
@@ -26,5 +27,20 @@ const filename = (ext) => isDev
              },
          }),
          new CleanWebpackPlugin(),
-     ]
+         new MiniCssExtractPlugin({
+             filename: `./css/${filename('css')}`
+         })
+     ],
+     module:{
+         rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+              },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+         ]
+     }
   };
