fe626d77e7d936fcdd59a6a9ee6767aae698ab96
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-11-04 16:41:53 +0200

    Add startup settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,75 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const ImageminPlugin = require("imagemin-webpack");
 
 
 const isDev = process.env.NODE_ENV === "development";
 const isProd = !isDev;
+
+const optimization = () => {
+    const configObj = {
+     splitChunks: {
+         chunks: 'all'
+     }
+    };
+    if (isProd) {
+        configObj.minimizer = [
+         new OptimizeCssAssetsWebpackPlugin (),
+         new TerserWebpackPlugin()
+        ];
+    }
+    return configObj;
+};
+
+const plugins = () => {
+    const basePlugins = [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            filename: 'index.html',
+            minify: {
+                collapseWhitespace: isProd,
+            },
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${filename('css')}`
+        })
+    ];
+     if(isProd){
+         basePlugins.push(
+    new ImageminPlugin({
+        bail: false, // Ignore errors on corrupted images
+        cache: true,
+        imageminOptions: {
+            // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+    
+            // Lossless optimization with custom option
+            // Feel free to experiment with options for better result for you
+            plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["jpegtran", { progressive: true }],
+            ["optipng", { optimizationLevel: 5 }],
+            [
+                "svgo",
+                {
+                plugins: [
+                    {
+                    removeViewBox: false
+                    }
+                ]
+                }
+            ]
+            ]
+        }
+        })
+         )
+     }
+    return basePlugins;
+}
 
 const filename = (ext) => isDev
     ? `[name].${ext}`
@@ -29,19 +93,8 @@ const filename = (ext) => isDev
         compress: true,
         port: 4444,
     },
-     plugins: [
-         new HTMLWebpackPlugin({
-             template: path.resolve(__dirname, 'src/index.html'),
-             filename: 'index.html',
-             minify: {
-                 collapseWhitespace: isProd,
-             },
-         }),
-         new CleanWebpackPlugin(),
-         new MiniCssExtractPlugin({
-             filename: `./css/${filename('css')}`
-         })
-     ],
+    optimization: optimization(),
+     plugins: plugins(),
      devtool: isProd ? false : "source-map",
      module:{
          rules: [
